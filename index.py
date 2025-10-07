8f0b1861a3e08f448c2010690a58c593ec042c75
EyadAyman01 <eyad.ayman.moh11@gmail.com>
2025-10-07 14:34:37 +0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,13 @@
+x = 30
+if x >= 30:
+    print("It's a hot day. Stay hydrated!")
+if x >20 and x <29:
+    print("It's a warm day. Engoy the weather!")    
+if x >19 and x <10:
+    print("It's a cool day. Wear a jacket!") 
+if x <10:
+    print("It's a cold day. Stay warm!")
+
+for num in range(1,21):
+    if num %3==0:
+        print(num)
